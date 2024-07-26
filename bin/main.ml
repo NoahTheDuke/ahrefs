07ee218f791fdc76130f45e4d3006fa729259a0a
Noah Bogart <noah.bogart@hey.com>
2024-07-26 16:47:18 -0400

    Cli in new file, better optionbag

--- bin/main.ml
+++ bin/main.ml
@@ -1,36 +1,7 @@
-module Cli = Getopt
-
-module OptionBag = struct
-  type t = {
-    username : string option;
-    port : string option;
-    args : string list;
-  }
-  let create () : t =
-    { username = None;
-      port = None;
-      args = []; }
-  let pp ppf {username; port; args} = 
-    Format.fprintf ppf "{username: %s; port: %s; args: [%s]}"
-      (Option.value username ~default:"<no username>")
-      (Option.value port ~default:"<12345>")
-      (String.concat " " args)
-end
-
-
-let specs (opts: OptionBag.t ref) =
-  [
-    ('u', "username", None, Some (fun x -> opts := { !opts with username = Some x }));
-    ('p', "port", None, Some (fun x -> opts := { !opts with port = Some x }));
-  ]
-
-let parse_argument (options : OptionBag.t ref) (x : string) =
-  options := { !options with args = !options.args @ [x]}
+module Cli = Nb_chatlib.Cli
 
 let main () =
-  let options = ref (OptionBag.create ()) in
-  let () = Cli.parse_cmdline (specs options) (parse_argument options) in
-  let options = !options in
-  OptionBag.pp Format.std_formatter options
+  (let options = (Cli.parse Sys.argv) in
+    (Cli.OptionBag.pp options))
 
-let () = main ()
+let () = (main ())
---
+++ lib/cli.ml
@@ -0,0 +1,40 @@
+module OptionBag = struct
+  type t = { username : string option; port : int option; mode : string }
+
+  let create () : t = { username = None; port = Some 8080; mode = "server" }
+
+  let to_string { username; port; mode } =
+    Printf.sprintf "{username: %s; port: %s; mode: [%s]}"
+      (Option.value username ~default:"<no username>")
+      (if Option.is_some port then string_of_int (Option.get port) else "<no port>")
+      mode
+
+  let pp bag = print_endline @@ to_string bag
+end
+
+let specs (opts : OptionBag.t ref) =
+  [
+    ('u', "username", None, Some (fun s -> opts := { !opts with username = Some s }));
+    ('p', "port", None, Some (fun s -> 
+      match int_of_string_opt s with
+      | None -> ()
+      | Some i -> opts := { !opts with port = Some i }));
+  ]
+
+(** [parse input] parses an array of strings into a OptionBag*)
+let parse input : OptionBag.t =
+  let options = ref (OptionBag.create ()) in
+  let modes : string list ref = ref [] in
+  let () =
+    Getopt.parse (specs options)
+      (fun x -> modes := !modes @ [ x ])
+      input 1
+      (Array.length input - 1)
+  in
+  match !modes with
+  | [] -> !options
+  | [ m ] -> { !options with mode = m }
+  | m ->
+      let mode_str = String.concat "; " m in
+      let msg = Printf.sprintf "Too many modes: [%s]" mode_str in
+      raise (Invalid_argument msg)
