8eeee29bc2961b4184c6adaa5778d1ed967db24f
Noah Bogart <noah.bogart@hey.com>
2024-07-26 17:07:23 -0400

    Use Variant for mode type

--- lib/cli.ml
+++ lib/cli.ml
@@ -1,13 +1,19 @@
 module OptionBag = struct
-  type t = { username : string option; port : int option; mode : string }
+  type t = {
+    username : string option;
+    port : int option;
+    mode : [ `Client | `Server ];
+  }
 
-  let create () : t = { username = None; port = Some 8080; mode = "server" }
+  let create () : t = { username = None; port = Some 8080; mode = `Server }
 
   let to_string { username; port; mode } =
     Printf.sprintf "{username: %s; port: %s; mode: [%s]}"
       (Option.value username ~default:"<no username>")
       (if Option.is_some port then string_of_int (Option.get port) else "<no port>")
-      mode
+      (match mode with
+      | `Client -> "Client"
+      | `Server -> "Server")
 
   let pp bag = print_endline @@ to_string bag
 end
@@ -15,10 +21,14 @@ end
 let specs (opts : OptionBag.t ref) =
   [
     ('u', "username", None, Some (fun s -> opts := { !opts with username = Some s }));
-    ('p', "port", None, Some (fun s -> 
-      match int_of_string_opt s with
-      | None -> ()
-      | Some i -> opts := { !opts with port = Some i }));
+    ( 'p',
+      "port",
+      None,
+      Some
+        (fun s ->
+          match int_of_string_opt s with
+          | None -> ()
+          | Some i -> opts := { !opts with port = Some i }) );
   ]
 
 (** [parse input] parses an array of strings into a OptionBag*)
@@ -33,7 +43,15 @@ let parse input : OptionBag.t =
   in
   match !modes with
   | [] -> !options
-  | [ m ] -> { !options with mode = m }
+  | [ m ] ->
+      {
+        !options with
+        mode =
+          (match String.lowercase_ascii m with
+          | "server" -> `Server
+          | "client" -> `Client
+          | _ -> `Server);
+      }
   | m ->
       let mode_str = String.concat "; " m in
       let msg = Printf.sprintf "Too many modes: [%s]" mode_str in
