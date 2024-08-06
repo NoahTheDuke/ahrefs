5270f490228012cc7b9df35ab793ab5b6a304698
Noah Bogart <noah.bogart@hey.com>
2024-08-06 12:45:58 -0400

    Complete enough, I think

--- lib/message.ml
+++ lib/message.ml
@@ -1,15 +1,28 @@
 module Message = struct
-  type t = Ping | Contents of string
+  type t =
+    | Ping
+    | Handshake of string
+    | Contents of string
 
   let to_string = function
     | Ping -> "<acknowledged>"
+    | Handshake s -> Printf.sprintf "<handshake> %s" s
     | Contents s -> s
 
   let to_bytes = function
     | Ping -> "<acknowledged>" |> Bytes.of_string
+    | Handshake s -> Printf.sprintf "<handshake> %s" s |> Bytes.of_string
     | Contents s -> s |> Bytes.of_string
 
   let of_string = function
     | "<acknowledged>" -> Ping
-    | s -> Contents s
+    | str -> (
+        match String.starts_with ~prefix:"<handshake> " str with
+        | true -> Handshake (String.sub str 12 ((String.length str) - 12))
+        | false -> Contents str)
+
+  let msg_type = function
+    | Ping -> "Ping"
+    | Handshake _ -> "Handshake"
+    | Contents _ -> "Contents"
 end
--- lib/server.ml
+++ lib/server.ml
@@ -2,8 +2,6 @@ open Lwt.Syntax
 open User
 open Message
 
-let ( >>= ) = Lwt.( >>= )
-
 let print_new_connection (client_sockaddr : Unix.sockaddr) =
   Lwt_io.printf "Connected from: %s\n"
     (match client_sockaddr with
@@ -11,41 +9,42 @@ let print_new_connection (client_sockaddr : Unix.sockaddr) =
         Printf.sprintf "%s:%i" (Unix.string_of_inet_addr addr) port
     | Unix.ADDR_UNIX a -> a)
 
-let get_message fd =
+let get_message client_socket =
   let buf = Bytes.create 2048 in
-  let* len = Lwt_unix.read fd buf 0 2048 in
-  let msg =
-    buf |> Bytes.to_string |> String.to_seq
-    |> Seq.filter (fun s -> not (Char.equal s '\000'))
-    |> String.of_seq |> String.trim |> Message.of_string
-  in
+  let* len = Lwt_unix.read client_socket buf 0 2048 in
+  let msg = Bytes.sub_string buf 0 len |> String.trim in
+  let msg = Message.of_string msg in
   Lwt.return (len, msg)
 
 let sockaddr_for_user (user : User.t) =
   Lwt_unix.ADDR_INET (Unix.inet_addr_loopback, user.port)
 
-let send_message (user : User.t) fd (msg : Message.t) =
+let send_message (user : User.t) client_socket (msg : Message.t) =
   let msg_str =
     match msg with
-    | Message.Ping -> Message.to_string msg
-    | Message.Contents s -> Printf.sprintf "%s: %s\n" user.username s
+    | Message.Contents s ->
+        user.ping <- Unix.gettimeofday ();
+        Printf.sprintf "%s: %s\n" user.username s
+    | m -> Message.to_string m
   in
   let bmsg = Bytes.of_string msg_str in
   let msg_len = Bytes.length bmsg in
-  let _ =
-    match msg with
-    | Message.Contents _ -> user.ping <- Unix.gettimeofday ()
-    | _ -> ()
-  in
-  let* _ = Lwt_unix.write fd bmsg 0 msg_len in
+  let* _ = Lwt_unix.write client_socket bmsg 0 msg_len in
   Lwt.return_unit
 
-let close_socket (user : User.t) =
+let close_socket (user : User.t) client_socket =
+  let* _ =
+    match user.mode with
+    | `Server -> Lwt.return_unit
+    | `Client ->
+        send_message user client_socket
+          (Printf.sprintf "%s has disconnected." user.username |> Message.of_string)
+  in
   let* _ = Lwt_unix.close user.socket in
   let* _ = Lwt_io.printl "Server closed. Exiting..." in
   exit 0
 
-let rec handle_outgoing_message (user : User.t) fd =
+let rec handle_outgoing_message (user : User.t) client_socket =
   Lwt.catch
     (fun _ ->
       let* inp = Lwt_io.read_line_opt Lwt_io.stdin in
@@ -53,48 +52,66 @@ let rec handle_outgoing_message (user : User.t) fd =
       | None -> Lwt.return_unit
       | Some s ->
           let msg = Message.of_string s in
-          let* _ = send_message user fd msg in
-          handle_outgoing_message user fd)
-    (fun _ -> close_socket user)
+          let* _ = send_message user client_socket msg in
+          handle_outgoing_message user client_socket)
+    (fun exn -> Lwt.fail exn)
 
 let roundtrip_time (user : User.t) : float = Unix.gettimeofday () -. user.ping
 
-let rec handle_incoming_message (user : User.t) fd =
-  let* len, msg = get_message fd in
-  match len with
-  | 0 -> close_socket user
-  | _ ->
-      Lwt.catch
-        (fun _ ->
-          (match msg with
-          | Message.Ping -> Lwt_io.printlf "Ping took %f sec" (roundtrip_time user)
-          | Message.Contents msg ->
-              let* _ = send_message user fd Message.Ping in
-              Lwt_io.printl msg)
-          >>= fun _ -> handle_incoming_message user fd)
-        (fun _ -> close_socket user)
+let rec handle_incoming_message (user : User.t) client_socket =
+  match Lwt_unix.state client_socket with
+  | Lwt_unix.Opened -> (
+      let* len, msg = get_message client_socket in
+      match len with
+      | 0 -> (
+          match user.mode with
+          | `Server -> Lwt.return_unit
+          | `Client -> close_socket user client_socket)
+      | _ ->
+          Lwt.catch
+            (fun _ ->
+              let* _ =
+                match msg with
+                | Message.Ping ->
+                    Lwt_io.printlf "Roundtrip took %f sec" (roundtrip_time user)
+                | Message.Handshake s -> Lwt_io.printlf "%s has connected" s
+                | Message.Contents msg ->
+                    let* _ = send_message user client_socket Message.Ping in
+                    Lwt_io.printl msg
+              in
+              handle_incoming_message user client_socket)
+            (fun exn -> Lwt.fail exn))
+  | _ -> close_socket user client_socket
 
 let create_server (user : User.t) =
   let sockaddr = sockaddr_for_user user in
   let _ = Lwt_unix.bind user.socket sockaddr in
   Lwt_unix.listen user.socket 5;
   let rec loop () =
-    let* fd, sockaddr = Lwt_unix.accept user.socket in
-    let* _ = print_new_connection sockaddr in
-    Lwt.choose [ handle_outgoing_message user fd; handle_incoming_message user fd ]
-    >>= fun _ -> loop ()
+    let* client_socket, _client_sockaddr = Lwt_unix.accept user.socket in
+    let* _ =
+      Lwt.pick
+        [
+          handle_incoming_message user client_socket;
+          handle_outgoing_message user client_socket;
+        ]
+    in
+    loop ()
   in
   loop ()
 
 let create_client (user : User.t) =
   let sockaddr = sockaddr_for_user user in
-  let _ = Lwt_unix.connect user.socket sockaddr in
+  let* _ = Lwt_unix.connect user.socket sockaddr in
+  let* _ = send_message user user.socket (Message.Handshake user.username) in
   let rec loop () =
-    Lwt.choose
-      [
-        handle_outgoing_message user user.socket;
-        handle_incoming_message user user.socket;
-      ]
-    >>= fun _ -> loop ()
+    let* _ =
+      Lwt.pick
+        [
+          handle_incoming_message user user.socket;
+          handle_outgoing_message user user.socket;
+        ]
+    in
+    loop ()
   in
   loop ()
